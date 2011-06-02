ea19c0e46f47160baf0f19c85945176f919002f4
Oleg Mirzov <oleg.mirzov@gmail.com>
2011-06-02 20:38:44 +0200

    Added the improveSignals functionality for the Ex/Em signal reports (with tests)

--- src/main/scala/se/lu/chemphys/sms/brightstat/MolStat.scala
+++ src/main/scala/se/lu/chemphys/sms/brightstat/MolStat.scala
@@ -2,6 +2,7 @@ package se.lu.chemphys.sms.brightstat
 
 import java.io.PrintStream
 import scala.collection.immutable.TreeMap
+import scala.collection.mutable.ArrayBuffer
 class MolStat {
 	var x = 0
 	var y = 0
@@ -40,14 +41,8 @@ class BrightStat {
 			out println v.map(molstat => toIntens(molstat).formatted("%.2f")).mkString("\t")
 		}
 	}
-	
-	def printIntensityReport(out: PrintStream){
-		printAnyIntensityReport(out, _.I)
-	}
-	
-	def printBackgroundReport(out: PrintStream){
-		printAnyIntensityReport(out, _.background)
-	}
+	def printIntensityReport(out: PrintStream) = printAnyIntensityReport(out, _.I)
+	def printBackgroundReport(out: PrintStream) = printAnyIntensityReport(out, _.background)
 	
 	def printCoordinatesReport(frame: Int, out: PrintStream){
 		out.println("Molecule\tX\tY")
@@ -62,11 +57,61 @@ class BrightStat {
 		printCoordinatesReport(minFrame, out)
 	}
 	
-	def printExSignalsReport(out: PrintStream){
-		for((_, signal) <- exSignalsSilo) out.println(signal)
+	private def printSignalsReport(out: PrintStream, silo:TreeMap[Int, Double]){
+		import BrightStat._
+		improveSignals(silo.values.toArray).foreach(out.println(_))
 	}
+	def printExSignalsReport(out: PrintStream) = printSignalsReport(out, exSignalsSilo)
+	def printEmSignalsReport(out: PrintStream) = printSignalsReport(out, emSignalsSilo)
+}
+
+object BrightStat{
+	import scala.math.{abs, sqrt, min}
 	
-	def printEmSignalsReport(out: PrintStream){
-		for((_, signal) <- emSignalsSilo) out.println(signal)
+	def getStepStat(sigs: Array[Double]): (Double, Double) = {
+		val n = sigs.length
+		assert(n > 1, "There must be at least 2 signal values to calculate step statistics.")
+		val steps = sigs.sliding(2).map(arr => abs(arr(0) - arr(1))).toArray
+		val aver = steps.sum / (n - 1)
+		val sqAver = steps.map(step => step * step).sum / (n - 1)
+		val std = sqrt(sqAver - aver * aver)
+		(aver, std)
+	}
+  
+	def improveSignals(sigs: Array[Double]): Array[Double] = {
+	   val (stepAver, stepStd) = getStepStat(sigs)
+	   var N = sigs.length
+	   var starts = new ArrayBuffer[Int](); var stops = new ArrayBuffer[Int]()
+	   var i = 0; var j = 0
+	   while(i < N-1)
+	   {
+	      if(sigs(i+1) - sigs(i) > stepAver + 2.5 * stepStd)
+	      {
+	    	  j = i
+	          while(sigs(j+1) > sigs(j) && j >= 0) {j -= 1}
+	          starts += (if(j>=0) j + 1 else 0)
+	          while(sigs(i+1) > sigs(i) && i < N-1){i += 1}
+	      }
+	      if(sigs(i) - sigs(i+1) > stepAver + 2.5 * stepStd)
+	      {
+	    	  j = i
+	          while(sigs(j+1) < sigs(j) && j < N-1){j += 1}
+	          if(j < N-1) {stops += j }
+	          i=j
+	      }
+	      if(stops.length == starts.length - 1) stops += (N - 1)
+	      i += 1
+	   }
+	   j = 0; var subtr = 0d; i = 0
+	   val improved = sigs.clone
+	   while(i < starts.length)
+	   {
+	       while(j < starts(i)){improved(j)=0; j += 1};
+	       subtr = min(improved(starts(i)), improved(stops(i)))
+	       while(j <= stops(i)){improved(j) -= subtr; j += 1}
+	       i += 1
+	   }
+	   while(j < N) {improved(j) = 0; j += 1}
+	   improved
 	}
 }
---
+++ src/test/scala/se/lu/chemphys/sms/brightstat/tests/BrightStatTests.scala
@@ -0,0 +1,26 @@
+package se.lu.chemphys.sms.brightstat.tests
+
+import se.lu.chemphys.sms.brightstat._
+import org.junit.Test
+import org.junit.Assert._
+import org.scalatest.junit.JUnitSuite
+
+class BrightStatTests extends JUnitSuite{
+	import BrightStat._
+
+	@Test def signalStepStatTest(){
+		assertEquals((1, 0), getStepStat(Array[Double](0, 1, 2, 3)))
+		assertEquals((1.5, 0.5), getStepStat(Array[Double](0, 1, 3)))
+		var (aver, dev) = getStepStat(Array[Double](0, 1, 3, -2, 5, 8))
+		assertEquals(3.6, aver, 0.0001)
+		assertEquals(2.154, dev, 0.0001)
+	}
+	
+	@Test def improveSignalsTest(){
+		val sigs = Array.fill(30)(-0.1d)
+		sigs(10) = 10; sigs(11) = 10
+		val improved = improveSignals(sigs).zipWithIndex.filter(_._1 > 0).map(_._2)
+		assertArrayEquals(Array(10, 11), improved)
+	}
+	
+}
