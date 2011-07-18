dbbb4e698411623c75f5feadbd825b745075d7b7
Oleg Mirzov <oleg.mirzov@gmail.com>
2011-07-18 23:23:39 +0200

    Fixed issue #1: ROI rectangle update when window is resized

--- src/main/scala/se/lu/chemphys/sms/brightstat/ui/MovieScreen.scala
+++ src/main/scala/se/lu/chemphys/sms/brightstat/ui/MovieScreen.scala
@@ -1,33 +1,60 @@
 package se.lu.chemphys.sms.brightstat.ui
 import java.awt.geom.Ellipse2D.{Double => Ellipse}
 import java.awt.geom.Point2D.{Double => Point}
-import java.awt.geom.{AffineTransform, Point2D}
-import java.awt.image.BufferedImage
+import java.awt.geom.AffineTransform
+import java.awt.geom.Point2D
 import java.awt.Graphics2D
-import _root_.scala.swing.{BorderPanel, event}
+import java.awt.Rectangle
+
+import scala.swing.BorderPanel
+import scala.swing.event
+
+import MovieScreen.lookup
+import MovieScreen.rectToROI
+import MovieScreen.roiToRect
+import MovieWidget.point2ints
+import se.lu.chemphys.sms.brightstat.NoROI
 import se.lu.chemphys.sms.brightstat.ROI
 
-class MovieScreen(widget: MovieWidget, state: StateManager) extends BorderPanel {
-	import MovieWidget._
-	import widget._
+object MovieScreen{
 
-	var molsToShow: Seq[(Int, Int)] = Array[(Int, Int)]()
-//	def molsToShow = _molsToShow
-//	def molsToShow_=(mols: Seq[(Int, Int)]){_molsToShow = mols; showDetectedMols = true}
-	var showDetectedMols = false
-	
-	def transform : AffineTransform = {
-		val width = size.width
-		val height = size.height
-		val scaleX = width.toDouble / image.getWidth
-		val scaleY = height.toDouble / image.getHeight
-		new AffineTransform(scaleX, 0, 0, scaleY, 0, 0)
-	}
-	
-	def lookup(pixel: Point2D) = {
+	def lookup(pixel: Point2D, transform: AffineTransform): (Int, Int) = {
 		val res = transform.inverseTransform(pixel, new Point())
 		(res.getX.toInt, res.getY.toInt)
 	}
+
+	def rectToROI(rect: Rectangle, transform: AffineTransform): ROI = {
+		val leftTop = rect.getLocation
+		val (left, top) = lookup(leftTop, transform)
+		val rightBottom = new Point(leftTop.getX + rect.getWidth, leftTop.getY + rect.getHeight)
+		val (right, bottom) = lookup(rightBottom, transform)
+		ROI(left, top, right, bottom)
+	}
+	
+	def roiToRect(roi: ROI, transform: AffineTransform): Rectangle = {
+		val leftTop = transform.transform(new Point(roi.left, roi.top), new Point())
+		val (left, top) = (leftTop.getX.toInt, leftTop.getY.toInt)
+		val rightBottom = transform.transform(new Point(roi.right, roi.bottom), new Point())
+		val (right, bottom) = (rightBottom.getX.toInt, rightBottom.getY.toInt)
+		new Rectangle(left, top, right-left, bottom - top)
+	}
+}
+
+class MovieScreen(widget: MovieWidget, state: StateManager) extends BorderPanel {
+	import MovieWidget._
+	import MovieScreen._
+	import widget._
+
+	var molsToShow: Seq[(Int, Int)] = Array[(Int, Int)]()
+	var showDetectedMols = false
+	
+	def transform : AffineTransform = {
+		val scaleX = size.width.toDouble / image.getWidth
+		val scaleY = size.height.toDouble / image.getHeight
+		new AffineTransform(scaleX, 0, 0, scaleY, 0, 0)
+	}
+	
+	def toCameraPixel(pixel: Point2D): (Int, Int) = lookup(pixel, transform)
 	
 	override def paint(g: Graphics2D){
 		val curTransf = transform
@@ -50,33 +77,30 @@ class MovieScreen(widget: MovieWidget, state: StateManager) extends BorderPanel 
 	}
 	
 	private val roirect = new java.awt.Rectangle()
+	private var lastRoi: ROI = NoROI
 	private def getRoi: ROI = {
-		val leftTop = roirect.getLocation
-		val (left, top) = lookup(leftTop)
-		val rightBottom = new Point(leftTop.getX + roirect.getWidth, leftTop.getY + roirect.getHeight)
-		val (right, bottom) = lookup(rightBottom)
-		ROI(left, top, right, bottom)
+		lastRoi = rectToROI(roirect, transform); lastRoi
 	}
 	
-	listenTo(mouse.clicks)
+	listenTo(mouse.clicks, this)
 	reactions += {
 		case down: event.MousePressed  =>
 		  	val (x, y) = point2ints(down.point)
 			roirect.setBounds(x, y, 0, 0)
 			state ! "selectroi"
-			//println (down)
 		case drag: event.MouseDragged =>
 		  	val (x, y) = point2ints(drag.point)
 		  	roirect.add(x, y)
-		  	repaint
-		  	//println(drag)
+		  	repaint()
 		case up: event.MouseReleased =>
 		  	if(point2ints(up.point) == point2ints(roirect.getLocation)){
 		  		state ! "noroi"
 		  		showRoi = false
-		  		repaint
+		  		repaint()
 		  	} else {state ! getRoi}
 		  	state ! "finishselectingroi"
-		  	//println(up)
+		case resize: event.UIElementResized if(lastRoi != NoROI) =>
+			roirect.setBounds(roiToRect(lastRoi, transform))
+			repaint()
 	}
 }
